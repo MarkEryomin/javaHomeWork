5389963ff113b39b97ec7aa43d4a360594f28921
Mark <erjomin.mark@gmail.com>
2023-05-01 02:33:39 +0300

    end home work seminar 1

--- src/main/java/org/example/seminar1/task1/Main.java
+++ src/main/java/org/example/seminar1/task1/Main.java
@@ -2,6 +2,22 @@ package org.example.seminar1.task1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!hgghjkn");
+        System.out.println(reverseWords("Hello , my world!"));
+    }
+
+
+    /**
+     *
+     * @param str изначальная строка
+     * @return строку со словами в обратном порядке
+     */
+    public static String reverseWords(String str) {
+        String[] words = str.split(" ");
+        String res = "";
+        for (int i = words.length-1; i >= 0; i--) {
+            res = res + " " + words[i];
+        }
+        return res.trim();
+
     }
 }
