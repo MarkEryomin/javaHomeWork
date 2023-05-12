bffc60c9d8d67b4367a21f678900e6a4b630fbf9
Mark <erjomin.mark@gmail.com>
2023-05-13 00:38:33 +0300

    end 'HomeWork4'

---
+++ src/main/java/org/example/seminar4/HW1/ListNode.java
@@ -0,0 +1,9 @@
+package org.example.seminar4.HW1;
+
+ public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
